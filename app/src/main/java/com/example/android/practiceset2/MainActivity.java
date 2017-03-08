77964f03d33a7a77dab96e94eaaad68523069f2f
Yogesh Jariwala <yogesh.jariwala@riba.org>
2017-03-08 20:15:00 +0000

    TravelTimes variables and testing

--- app/src/main/java/com/example/android/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.android.practiceset2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // PASTE CODE YOU WANT TO TEST HERE
+        int day1 = 15;
+        int day2 = 22;
+        int day3 = 18;
+        display((day1 + day2 + day3) / 3);
     }
+
+    /**
+     * Display methods that allow the text to appear on the screen. Don't worry if you don't know
+     * how these work yet. We'll be covering them in lesson 3.
+     */
+
+    public void display(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text);
+    }
+
+    public void display(int text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text + "");
+    }
+
+
+
+
 }
